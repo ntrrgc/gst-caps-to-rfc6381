ed2868b6253e5d965f164ca1a4401dbe93354e32
Alicia Boya García <aboya@igalia.com>
2025-08-05 11:54:10 +0200

    Periods and newlines

--- gst-caps-to-rfc6381.c
+++ gst-caps-to-rfc6381.c
@@ -15,12 +15,12 @@ main (int   argc,
 
   GstCaps* caps = gst_caps_from_string (argv[1]);
   if (!caps) {
-    g_printerr("Failed to create caps.");
+    g_printerr("Failed to create caps.\n");
     return -1;
   }
   gchar* mime_codec = gst_codec_utils_caps_get_mime_codec (caps);
   if (!mime_codec) {
-    g_printerr ("Could not create a MIME codec string");
+    g_printerr ("Could not create a MIME codec string.\n");
     return -1;
   }
 
